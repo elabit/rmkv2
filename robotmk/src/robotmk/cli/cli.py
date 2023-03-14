595125b9c7bb3f557cd1195ac6413c558a347c75
Simon Meggle <simon.meggle@tribe29.com>
2023-03-14 12:06:19 +0100

    Added tests

--- robotmk/src/robotmk/cli/cli.py
+++ robotmk/src/robotmk/cli/cli.py
@@ -9,50 +9,47 @@ import os.path
 from functools import wraps
 
 
-# def add_parent(parent):
-#     def decorator(func):
-#         func.parent = parent
-#         return func
-
-#     return decorator
-
-
-# @add_parent("my_parent")
-# def my_function():
-#     pass
-
-
-# print(my_function.parent)  # prints "my_parent"
-
-
-# CMD1        CMD2     CMD3         OPT1 OPT2 OPT3
+# CMD1        CMD2     OPTION                                    CMD3            # Description
 # ---------------------------------------------------------------------------------------------
 # # LOCAL CONTEXT
 # robotmk                                                                        # no arg = print output
 
-# robotmk     local    output                                                    # print output
-# robotmk     local    output       --yml /etc/checkmk/another_robotmk.yml       # print output with yml
-# robotmk     local    output       --vars /var/robotmk_local.env                # print output, load env from file (instead of env)
+# robotmk     local                                              output          # print output
+# robotmk     local    --yml /etc/checkmk/another_robotmk.yml    output          # print output with yml
+# robotmk     local    --vars /var/robotmk_local.env             output          # print output, load env from file (instead of env)
 
 # robotmk     local    scheduler                                                 # start scheduler
-# robotmk     local    scheduler    --yml /etc/checkmk/another_robotmk.yml       # start scheduler with yml
-# robotmk     local    scheduler    --vars /var/robotmk_local.env                # start scheduler, load env from file  (instead of env)
+# robotmk     local    --yml /etc/checkmk/another_robotmk.yml    scheduler       # start scheduler with yml
+# robotmk     local    --vars /var/robotmk_local.env             scheduler       # start scheduler, load env from file  (instead of env)
 # ---------------------------------------------------------------------------------------------
 # # SUITE CONTEXT
 # robotmk                                                                        # no arg = exec suite as configured in env
-# robotmk                           --vars /var/rmk/foosuiteA_8bb36c3.env  foobarsuiteA   # exec suite with env from file
-# robotmk     suite    vardump         foobarsuiteA                                 # dump vars for foobarsuiteA
+# robotmk     suite    --vars /var/rmk/foosuiteA_8bb36c3.env                     # exec suite with env from file and suite = yml -> common: suite)
+# robotmk     suite    --vars /var/rmk/foosuiteA_8bb36c3.env     bazsuite        # exec suite with env from file and suite = bazsuite
+# robotmk     suite                                              vardump  foobarsuiteA   # just dump the vars for foobarsuiteA
 # ---------------------------------------------------------------------------------------------
 # # SPECIALAGENT (="s.a."") CONTEXT
 # robotmk                                                                        # no arg = seq & output
 # (robotmk    s.a.     output)                                                   # NOT POSSIBLE - no config file
-# robotmk     s.a.     output      --vars ~/var/robotmk/s.a.-hostfoo.env         # run output with env from file
+# robotmk     s.a.     --vars ~/var/robotmk/s.a.-hostfoo.env     output          # run output with env from file
 
 # (robotmk    s.a.     sequencer)                                                # NOT POSSIBLE - no config file
-# robotmk     s.a.     sequencer   --vars ~/var/robotmk/s.a.-hostfoo.env         # run requencer with env from file
+# robotmk     s.a.     --vars ~/var/robotmk/s.a.-hostfoo.env     sequencer       # run requencer with env from file
 
 
-def get_commands_from_pkg(pkg, root=None) -> dict:
+def get_commands_from_pkg(pkg) -> dict:
+    """CLI options for the contexts are defined within the context packages within cli.py module.
+
+    Each sub-PACKAGE of pkg is considered a sub-COMMAND. This function recursively traverses the
+    package tree and returns all function objects which have the same name as the package.
+
+    This allows to maintain the CLI logic within the context packages, while still having a single
+    entry point for the CLI.
+
+    Example:
+    robotmk.context.local.cli.py contains a function called local(), decorated with @click.group()
+    and connected with subcomands "output()" and "scheduler()". The discovered subcommand is "local()".
+    """
     pkg_obj = importlib.import_module(pkg)
     pkg_path = os.path.dirname(pkg_obj.__file__)
     commands = {}
@@ -60,25 +57,9 @@ def get_commands_from_pkg(pkg, root=None) -> dict:
         module_obj = importlib.import_module(f"{pkg}.{module.name}")
         if module.ispkg:
             cmd_from_pkg = get_commands_from_pkg(f"{pkg}.{module.name}")
-            # commands[module.name.replace("_", "-")] = cmd_from_pkg
             commands.update(cmd_from_pkg)
-            # commands[module.name.replace("_", "-")] = click.Group(
-            #     context_settings={"help_option_names": ["-h", "--help"]},
-            #     help=module_obj.__doc__,
-            #     commands=cmd_from_pkg,
-            # )
-            pass
-            # did we get any commands?
-            # commands[module.name]
         else:
             if module.name == "cli":
-                # within cli.py, list all custom functions and add them as commands
-                # TODO: determine the list of functions to ignore dynamically
-                # cli_functions = [
-                #     f
-                #     for f in dir(module_obj)
-                #     if (f not in ["click"]) and not f.startswith("__")
-                # ]
                 cli_functions = [f for f in dir(module_obj) if f == pkg.split(".")[-1]]
 
                 for cli_function in cli_functions:
@@ -87,6 +68,7 @@ def get_commands_from_pkg(pkg, root=None) -> dict:
     return commands
 
 
+# Create the main group and assign the subcommands gathered from the context packages
 @click.group(
     context_settings={"help_option_names": ["-h", "--help"]},
     help=__doc__,
@@ -96,41 +78,14 @@ def get_commands_from_pkg(pkg, root=None) -> dict:
 @click.pass_context
 def main(ctx):
     if ctx.invoked_subcommand is None:
+        # Executing Robotmk without arguments means to run Robotmk in the contexts default mode.
         ctx.robotmk = Robotmk()
         ctx.robotmk.load_config(DEFAULTS)
         ctx.robotmk.run_default()
     else:
+        # Robotmk was executed with a context subcommand, whose logic is defined within the context.
         pass
 
-
-# root_commands = get_commands_from_pkg("robotmk.context")
-
-
-# @click.group("robotmk", invoke_without_command=True)
-# @click.pass_context
-# def main(ctx):
-#     print(__name__ + ": " + "(cli main)")
-#     if ctx.invoked_subcommand is None:
-#         print("no subcommand")
-#     else:
-#         pass
-
-
-# @main.command()
-# @click.option("--list", is_flag=True, help="List all contexts")
-# def context(list):
-#     if list:
-#         click.echo("List all contexts")
-#         click.echo("local")
-#         click.echo("specialagent")
-#         click.echo("suite")
-
-
-# context = "local"
-# robotmk = Robotmk(context)
-# robotmk.load_config(DEFAULTS)
-# robotmk.run()
-# pass
 
 if __name__ == "__main__":
     main()
