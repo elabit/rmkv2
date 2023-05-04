2acb1a1399e02bb31a277514d4981213a724ebe4
Simon Meggle <simon.meggle@tribe29.com>
2023-05-04 12:38:59 +0200

    Updated release script

--- robotmk/release.sh
+++ robotmk/release.sh
@@ -1,16 +1,48 @@
-PKG_ROOT=/home/simonmeggle/Documents/01_dev/rmkv2/robotmk
+#!/bin/bash
 
-msg=$1
+PKG_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-pushd $PKG_ROOT
+function usage() {
+    echo "Usage: $0 <part> [<commit message>]"
+    echo "  <commit message> - commit message for the release"
+    exit 1
+}
 
-git add .. &&
-    git commit -m "$msg" &&
-    bumpversion patch --tag --commit &&
-    # git add . &&
-    # git commit -m "Bumped version" &&
-    git push && git push --tags
+# check for two arguments
+
+if [ $# -lt 1 ]; then
+    usage
+else
+    part=$1
+    msg=$2
+fi
+
+pushd "$PKG_ROOT" || exit
+
+# check if there are any uncommitted changes
+if [[ $(git status -s) ]]; then
+    if [ "$msg" == "" ]; then
+        echo "Git is dirty. Please provide a commit message. Exiting..."
+        exit 1
+    else
+        git add .. && git commit -m "$msg"
+    fi
+else
+    # git is clean
+    if [ "$msg" != "" ]; then
+        echo "A commmit message was provided, but git is clean already. Exiting..."
+        exit 1
+    else
+        echo "Git is clean. Proceeding..."
+    fi
+fi
+
+exit
+
+bumpversion $part --tag --commit &&
+    git push &&
+    git push --tags
 
 flit publish
 
-popd
+popd || exit
