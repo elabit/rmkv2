5b62e51a96f87db7823c6041fa378e46960a0784
Simon Meggle <simon.meggle@tribe29.com>
2023-05-04 12:42:32 +0200

    Hide server command until implemented

--- robotmk/src/robotmk/context/server/cli.py
+++ robotmk/src/robotmk/context/server/cli.py
@@ -6,12 +6,12 @@ import click
 from robotmk.main import Robotmk, DEFAULTS
 
 
-# use module docstring as help text
-@click.command(help=__doc__)
-@click.pass_context
-def server(ctx):
-    click.secho("server", fg="green")
-    click.echo("Serving the Robotmk webinterface on http://localhost:8099 ...")
-    # ctx.robotmk = Robotmk("server")
-    # ctx.obj.config.set("common.context", "server")
-    pass
+# # use module docstring as help text
+# @click.command(help=__doc__)
+# @click.pass_context
+# def server(ctx):
+#     click.secho("server", fg="green")
+#     click.echo("Serving the Robotmk webinterface on http://localhost:8099 ...")
+#     # ctx.robotmk = Robotmk("server")
+#     # ctx.obj.config.set("common.context", "server")
+#     pass
